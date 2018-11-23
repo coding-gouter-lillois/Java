7eb26e0222d823614933883f1cb5338413a1c446
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2018-11-24 00:16:43 +0100

    Add buged files with upload
    I started programming a pong game but there a bug and the Paddle isn't moving as it sould :/
    How to solve it ?!

---
+++ Vianney/pong/src/Pong/PlayerPaddle.java
@@ -0,0 +1,69 @@
+package Pong;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class PlayerPaddle implements Paddle{
+
+	double y;
+	double yVel;
+	boolean upAccel, downAccel;
+	int player, x;
+	final double INERTIA = 0.94;
+	
+	public PlayerPaddle(int player){
+		upAccel = false;
+		downAccel = false;
+		y = 210; yVel = 0;
+		
+		if(player == 1){
+			x = 20;
+		} else {
+			x = 660;
+		}
+		
+	}
+	
+	@Override
+	public void draw(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(x, (int)y, 20, 80);
+	}
+	
+
+	@Override
+	public void move() {
+		if(upAccel){
+			yVel -= 2;
+		} else if(downAccel){
+			yVel += 2;
+		} else if(!upAccel && !downAccel){
+			yVel *= INERTIA;
+		}
+		
+		if(yVel >= 5){
+			yVel =5;
+		} else if(yVel <= -5){
+			yVel = -5;
+		}
+		
+		//La raquette bouge
+		y += yVel;
+		System.out.println(yVel + "y = " + y);
+		
+	}
+
+	
+	public void setUpAccel(boolean input){
+		upAccel = input;
+	}
+	
+	public void setDownAccel(boolean input){
+		downAccel = input;
+	}
+
+	@Override
+	public int getY(){
+		return(int)y;
+	}
+}
---
+++ Vianney/pong/src/Pong/Tennis.java
@@ -0,0 +1,85 @@
+package Pong;
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Tennis extends Applet implements Runnable, KeyListener{
+	
+	private static final long serialVersionUID = 1L;
+
+	//On choisi la taille de la fenêtre
+	final int WIDTH = 700, HEIGHT = 500;
+	Thread thread;
+	
+	//Ajout d'une raquette
+	PlayerPaddle p1;
+	
+	public void init(){
+		//On crée la fenêtre selon les tailles choisies précedemment
+		this.resize(WIDTH, HEIGHT);
+		this.addKeyListener(this);
+		
+		p1 = new PlayerPaddle(1);
+		
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public void paint(Graphics g){
+		//On choisi la couleur du font d'écran
+		g.setColor(Color.black);
+		//On choisi l'espace rempli par la couleur choisie pour le font d'écran
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		p1.draw(g);
+	}
+	
+	public void update(Graphics g){
+		paint(g);
+	}
+
+	@Override
+	public void run() {
+		for(;;){
+			
+			p1.move();
+			
+			repaint();
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_UP){
+			p1.setUpAccel(true);
+		} else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+			p1.setDownAccel(true);
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_UP){
+			p1.setUpAccel(false);
+		} else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+			p1.setDownAccel(false);
+		}
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
